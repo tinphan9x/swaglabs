9afeb618a1da0517565f24571d5859218171ebe4
tinphan9x <86870659+tinphan9x@users.noreply.github.com>
2021-07-04 16:50:27 +0700

    update

--- Tests/TestRunner.py
+++ Tests/TestRunner.py
@@ -8,22 +8,8 @@ sys.path.append(os.getcwd())
 from unittest import TestLoader, TestSuite, TextTestRunner
 from Tests.Test_Login import Test_Login_Page
 
-import testtools as testtools
 
 if __name__ == "__main__":
-
-    # test_loader = TestLoader()
-    # # Test Suite is used since there are multiple test cases
-    # test_suite = TestSuite((
-    #     test_loader.loadTestsFromTestCase(Test_Login_Page),
-    #     ))
-    #
-    # test_runner = TextTestRunner(verbosity=2)
-    # test_runner.run(test_suite)
-    #
-    # # Refer https://testtools.readthedocs.io/en/latest/api.html for more information
-    # parallel_suite = testtools.ConcurrentStreamTestSuite(lambda: ((case, None) for case in test_suite))
-    # parallel_suite.run(testtools.StreamResult())
 
     os.system("C:/Users/USER/scoop/apps/allure/2.14.0/bin/allure generate --clean --output " + './Reports')
     #Run allure report of this file, export report to PJ/Reports
